6b4c23b61a9e7a37481f97677787c910b2627f8d
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-09-10 15:13:36 -0700

    move zeros simplified

--- Top-100-Liked-Questions/Array/283-Move_Zeros.py
+++ Top-100-Liked-Questions/Array/283-Move_Zeros.py
@@ -35,11 +35,22 @@ def move_zeroes(A):
             low += 1
     return A
 
+### simplified solutions
+def move_zeroes_1(A):
+    l = 0
+    for r in range(len(A)):
+        if A[r]:
+            if l != r: # to avoid unnecessary swapping
+                A[l], A[r] = A[r], A[l]
+            l += 1
+    return A
+
 if __name__ == '__main__':
     # nums = [0,0,0,2,0,1,3,5]
-    # nums = [2,0,1,3,5]
+    nums = [2,4,0,1,3,5]
     # nums = [0]
-    nums = [0, 1, 0, 3, 12]
+    # nums = [0, 1, 0, 3, 12]
     print(move_zeroes(nums))
+    print(move_zeroes_1(nums))
 
     
