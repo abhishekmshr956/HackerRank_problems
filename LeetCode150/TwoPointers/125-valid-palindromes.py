e18d873706cc5ec55865c1adb95082eb3739253b
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-09-20 09:45:49 -0700

    folder name changed

---
+++ LeetCode150/TwoPointers/125-valid-palindromes.py
@@ -0,0 +1,57 @@
+""" 
+https://leetcode.com/problems/valid-palindrome/
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters."""
+
+def isPalindrome(s):
+    
+    s_alphanumeric = ''
+    for c in s.lower():
+        if c in 'abcdefghijklmnopqrstuvwxyz0123456789':
+            s_alphanumeric += c
+    # print(s_alphanumeric)
+
+    n = len(s_alphanumeric)
+
+    i = 0
+    j = n - 1
+    while i < n//2:
+        if s_alphanumeric[i] != s_alphanumeric[j]:
+            return False
+        else:
+            i += 1
+            j -= 1
+    return True
+
+if __name__ == '__main__':
+    # s = "A man, a plan, a canal: Panama"
+    # s = "race a car"
+    s = " "
+    # s = "amanaplanacanalpanama"
+    print(isPalindrome(s))
