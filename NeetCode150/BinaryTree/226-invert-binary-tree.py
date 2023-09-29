fd04306c6aa0e50000bef0232d3ea2de96217476
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-09-29 15:51:09 -0700

    invert binary tree

--- NeetCode150/BinaryTree/226-invert-binary-tree.py
+++ NeetCode150/BinaryTree/226-invert-binary-tree.py
@@ -18,9 +18,7 @@ class Solution:
         if root == None:
             return
         else:
-            temp = root.left
-            root.left = root.right
-            root.right = temp
+            root.left, root.right = root.right, root.left
             self.invertTree(root.left)
             self.invertTree(root.right)
         return root
