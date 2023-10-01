5cae631708a390fe0328c90569a660e60b3df90f
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-10-01 12:05:46 -0700

    contains duplicate more solutions

--- YoutubeVideosCode/217-contains-duplicate.py
+++ YoutubeVideosCode/217-contains-duplicate.py
@@ -1,4 +1,23 @@
+#O(n^2)
+class Solution1:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+    
+#O(nlogn)
 class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+
+#O(n)
+class Solution2:
     def containsDuplicate(self, nums: list[int]) -> bool:
         uniqueElements = set()
         for n in nums:
@@ -7,6 +26,8 @@ class Solution:
             else:
                 uniqueElements.add(n)
         return False
+    
+
     
 if __name__ == '__main__':
     nums = [1,2,3,1] # true
