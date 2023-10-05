a37fef5ea9f0f7cee34a4a1a04960024b6eedd9e
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-10-05 13:49:47 -0700

    valid anagram

--- YoutubeVideosCode/242-Valid-Anagram.py
+++ YoutubeVideosCode/242-Valid-Anagram.py
@@ -12,8 +12,30 @@ class Solution:
 
         return charCount_s == charCount_t
     
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        charCount_s = {}
+
+        for char in s:
+            charCount_s[char] = charCount_s.get(char, 0) + 1
+
+        for char in t:
+            if char not in charCount_s or charCount_s[char] == 0:
+                return False
+            else:
+                charCount_s[char] -= 1
+
+        return True
+    
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+     
 if __name__ == '__main__':
-    s = "anagram"; t = "nagaram" #Output: true
+    s = "anagram"; t = "nagaram" # Output: true
     s = "rat"; t = "car" # Output False
     sol = Solution()
     print(sol.isAnagram(s, t))
