dfc324fbfd2363c1f5a4652eb832dfa479abbfac
abhishekmshr956 <abhishekmshr956@gmail.com>
2023-10-05 17:40:47 -0700

    group anagrams

--- YoutubeVideosCode/49-group-anagrams.py
+++ YoutubeVideosCode/49-group-anagrams.py
@@ -1,25 +1,44 @@
 from collections import defaultdict
 
+# time complexity O(n * k); n is len(strs); k is the maximum length of a string in the input 
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        res = defaultdict(list)
+        anagrams = {}
         
-        for s in strs:
+        for word in strs:
             counts = [0] * 26
 
-            for char in s:
+            for char in word:
                 counts[ord(char) - ord('a')] += 1
 
-            res[tuple(counts)].append(s)
+            key = tuple(counts)
 
-        return list(res.values())
+            if key not in anagrams:
+                anagrams[key] = [word]
+            else:
+                anagrams[key].append(word)
 
+        return list(anagrams.values())
+    
 
+# time complexity O(n * k * log(k)); n is len(strs); k is the maximum length of a string in the input    
+class Solution1:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = {}
 
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+
+        return list(anagrams.values())
 
 if __name__ == '__main__':
     strs = ["eat","tea","tan","ate","nat","bat"] # Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-    strs = [""] # Output: [[""]]
-    strs = ["a"] # Output: [["a"]]
+    # strs = [""] # Output: [[""]]
+    # strs = ["a"] # Output: [["a"]]
     sol = Solution()
     print(sol.groupAnagrams(strs))
